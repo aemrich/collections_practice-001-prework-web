535dc29e552d98d903a3a3636e619bd45f49cac2
aemrich <adam.emrich@gmail.com>
2015-08-26 22:08:28 -0400

    start of bonus collections

---
+++ collections_practice.rb
@@ -0,0 +1,34 @@
+def sort_array_asc(arr)
+  arr.sort
+end 
+
+def sort_array_desc(arr)
+  arr.sort do |a, b|
+    if a == b
+      0
+    elsif a > b
+      -1
+    elsif a < b
+      1
+    end
+  end
+end
+
+def swap_elements(arr)
+end
+
+def reverse_array(arr)
+end
+
+def kesha_maker(arr)
+end
+
+def find_a(arr)
+end
+
+def sum_array(arr)
+end
+
+def add_s(arr)
+end
+
